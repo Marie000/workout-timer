2242aadd847fc553019a4b6e78d949416f2774c2
Marie <marie.pelletier@gmail.com>
2016-04-07 11:32:05 -0400

    timer now also saves date, calories and distance to firebase database. Also fixed a few display problems

--- userData.js
+++ userData.js
@@ -4,23 +4,33 @@ var UserData = {
 	ref:new Firebase('https://workout-timer.firebaseio.com/'),
 	calories:null,
 	distance:null,
-	day:0,
-	month:0,
-	year:0,
+	date:0,
 	prompt:function(){
 		document.getElementById('resultTime').innerHTML=Workout.totalTime;
 		document.getElementById('resultDifficulty').innerHTML=Workout.difficulty;
 		document.getElementById('resultType').innerHTML=Workout.workoutType;
-		document.getElementById('modalButton').style.display="block";
+		document.getElementById('modalButton').style.display="inline-block";
 		this.getDate();
+		document.getElementById('date').innerHTML=this.date;
+
+	},
+	submitCalories: function(){
+		this.calories = document.getElementById('caloriesInput').value
+		document.getElementById('caloriesSection').innerHTML = this.calories;
+	},
+	submitDistance: function(){
+		this.distance = document.getElementById('distanceInput').value
+		document.getElementById('distanceSection').innerHTML = this.distance;
+	},
+	submitToDatabase: function(){
 		var database = this.ref.child('users').child('marie').child('workouts');
-		database.push({time:Workout.totalTime,difficulty:Workout.difficulty,type:Workout.workoutType})
+		database.push({time:Workout.totalTime,difficulty:Workout.difficulty,type:Workout.workoutType,
+			date:this.date,calories:this.calories,distance:this.distance})
+		document.getElementById('modal-body').innerHTML = "Data submitted."
 	},
 	getDate: function(){
 		var date = new Date();
-		this.day = date.getDate();
-		this.month = date.getMonth()+1;
-		this.year = date.getFullYear();
+		this.date = date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear();
 	}
 };
 
